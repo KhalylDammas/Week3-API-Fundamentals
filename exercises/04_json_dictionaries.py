6619ca445bd78555a9dfaf127c5f61169d05dda4
Sara <saragh8900@gmail.com>
2026-09-15 17:21:54 +0300

    Complete JSON dictionary exercise

--- exercises/04_json_dictionaries.py
+++ exercises/04_json_dictionaries.py
@@ -11,19 +11,23 @@ device = {
 }
 
 # TODO 1: Print the device name.
+print(device["name"])
 
 # TODO 2: Print the year.
+print(device["data"]["year"])
 
 # TODO 3: Print the CPU model.
+print(device["data"]["CPU model"])
 
 # TODO 4: Print the price.
+print(device["data"]["price"])
 
 # TODO 5: Update the price to a new value of your choice.
-
+device ["data"]["price"] = 3500
 # TODO 6: Add "available": True to the device dictionary.
-
+device ["avalible"] = True
 # TODO 7: Print the final dictionary.
-
+print(device)
 
 # Additional nested dictionary exercise
 student = {
@@ -35,7 +39,9 @@ student = {
 }
 
 # TODO 8: Print the student's name.
-
+print(student["name"])
 # TODO 9: Print the course title.
-
+print(student["course"]["title"])
 # TODO 10: Change the course week to 4, then print the final student dictionary.
+student["course"]["week"] = 4
+print(student)
